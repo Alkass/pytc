2746606dc02663ebbd80b89b84b9f10f44cb0d37
Al-Kass <Fadi_Hanna_Al-Kass@AMER.DELL.COM>
2015-12-25 01:57:47 -0600

    .

--- tests.py
+++ tests.py
@@ -2,11 +2,31 @@ from pytc.testcase import TestCase, run_tests
 
 class MyFirstTestCase(TestCase):
     enabled = True
-    description = "This is a basic test case"
+    description = "Your class description goes here"
+
+    def setup(self):
+        # our test cases preperation goes here
+        pass
+    setup.enabled = True
+    setup.description = "Your setup method description goes here"
 
     def test_1(self):
+        #Your test #1 goes here
         pass
     test_1.enabled = True
-    test_1.description = "Nothing to be done in test_1()"
+    test_1.description = "Your test description"
+
+    def test_2(self):
+        #Your test #2 goes here
+        pass
+    test_2.enabled = True
+    test_2.description = "Your test description"
+
+    def test_3(self):
+        #Your test #3 goes here
+        pass
+    test_3.enabled = True
+    test_3.description = "Your test description"
+
 
 run_tests(TestCase.__subclasses__(), debug_level=1)
